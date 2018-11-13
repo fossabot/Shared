fd7eb333b401b388ae96d556c13eb4538047f755
Daniel Crenna <daniel@hq.io>
2018-11-13 16:11:15 -0500

    reset should re-import sources and build them

--- reset.cmd
+++ reset.cmd
@@ -6,3 +6,12 @@ FOR /D %%p IN ("src\HQ.DocumentDb\*.*") DO rmdir "%%p" /s /q
 FOR /D %%p IN ("src\HQ.MySql\*.*") DO rmdir "%%p" /s /q
 FOR /D %%p IN ("src\HQ.Sqlite\*.*") DO rmdir "%%p" /s /q
 FOR /D %%p IN ("src\HQ.SqlServer\*.*") DO rmdir "%%p" /s /q
+
+REM need to move into the directory for MSBuild parameters to work
+cd src
+
+REM build once for merge source, and again for compile
+START /WAIT dotnet build -c Debug
+START /WAIT dotnet build -c Debug
+
+cd..
